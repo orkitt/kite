757dcb968f4b5af3315a1a83a17eee04a603537f
AR Rahman <orkitt.dev@gmail.com>
2026-03-07 00:42:52 +0600

    feat: added package icons

--- bin/kite.dart
+++ bin/kite.dart
@@ -1,14 +1,5 @@
 import 'package:args/command_runner.dart';
-
-import 'package:kite/commands/create_command.dart';
-import 'package:kite/commands/feature_command.dart';
-import 'package:kite/commands/model_command.dart';
-import 'package:kite/commands/route_command.dart';
-import 'package:kite/commands/widget_command.dart';
-// Code runner imports
-import 'package:kite/commands/run_commands.dart';
-import 'package:kite/commands/build_commands.dart';
-import 'package:kite/commands/generate_commands.dart';
+import 'package:kite/kite.dart';
 
 void main(List<String> args) {
   final runner = CommandRunner("lite", "Lite Flutter CLI")
--- lib/kite.dart
+++ lib/kite.dart
@@ -1,3 +1,10 @@
-int calculate() {
-  return 6 * 7;
-}
+
+export 'package:kite/commands/create_command.dart';
+export 'package:kite/commands/feature_command.dart';
+export 'package:kite/commands/model_command.dart';
+export 'package:kite/commands/route_command.dart';
+export 'package:kite/commands/widget_command.dart';
+// Code runner exports
+export 'package:kite/commands/run_commands.dart';
+export 'package:kite/commands/build_commands.dart';
+export 'package:kite/commands/generate_commands.dart';
